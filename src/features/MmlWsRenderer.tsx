54e0b98c358d73b985c4f1cdc6debff75d6bcb30
micha vie <micha@freeiam.com>
2023-10-02 21:09:01 +0200

    add lights to websocket renderer

--- src/features/MmlWsRenderer.tsx
+++ src/features/MmlWsRenderer.tsx
@@ -1,5 +1,6 @@
+import * as THREE from 'three'
 import React, { useEffect } from 'react'
-import { FullScreenMScene, RemoteDocumentWrapper, NetworkedDOMWebsocket, registerCustomElementsToWindow } from 'mml-web'
+import { FullScreenMScene, RemoteDocumentWrapper, NetworkedDOMWebsocket } from 'mml-web'
 
 type Props = {
   url: string
@@ -9,6 +10,13 @@ export function MmlWsRenderer(props: Props) {
   useEffect(() => {
     if (!props.url) return
     const fullScreenMScene = new FullScreenMScene()
+
+    const ambientLight = new THREE.AmbientLight(0x404040)
+    fullScreenMScene.getThreeScene().add(ambientLight)
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
+    directionalLight.position.set(0, 1, 1)
+    fullScreenMScene.getThreeScene().add(directionalLight)
 
     const camera = fullScreenMScene.getCamera()
     camera.position.set(-5, 5, 15)
